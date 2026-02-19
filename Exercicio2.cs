312078fb73df2216c6dbcc45f24899849709a4d6
EDUARDO CASTILHO DE PAULA <eduardo.paulo@portalsesisp.org.br>
2026-02-19 11:33:38 -0300

    entrega exercicio1, 2 e 3

---
+++ Exercicio2.cs
@@ -0,0 +1,11 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    private static string nome = "Eduardo Castilho";
+    
+    public static void Executar()
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
