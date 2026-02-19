9b3a4658e9863bc5bafb299de0ac3e73956a93c4
EDUARDO CASTILHO DE PAULA <eduardo.paulo@portalsesisp.org.br>
2026-02-19 15:01:26 -0300

    Exercicio 4, 5, 6 e 7

---
+++ Exercicio4.cs
@@ -0,0 +1,31 @@
+public static class Exercicio4
+{
+    public static void Executar()
+    {
+        Console.WriteLine();
+
+        Console.WriteLine("VSCODE"); 
+        Console.WriteLine();
+
+         string nome = "Eduardo Castilho";
+         string idade = "16";
+         string hobby1 = "Sinuca";
+         string hobby2 = "Esportes";
+         string graduação = "Agronomia";
+
+        //Console.WriteLine("nome");
+        
+        //Console.WriteLine("idade");
+        
+        //Console.WriteLine("hobby1");
+    
+        //Console.WriteLine("hobby2");
+        
+        //Console.WriteLine("graduação");
+       
+        Console.WriteLine($"Oi meu nome é {nome}, tenho {idade} anos, meus dois passatempos favoritos são {hobby1} e {hobby2} e pretendo cursar a graduação em {graduação}.");
+        Console.WriteLine();
+
+
+    }
+}
---
+++ Exercicio5.cs
@@ -0,0 +1,19 @@
+public static class Exercicio5
+{
+    public static void Executar()
+    {
+        Console.WriteLine();
+
+        Console.WriteLine("VSCODE"); 
+        Console.WriteLine();
+
+         string nome = "o Eduardo Castilho";
+         string estilo = "Sertanejo/Modão";
+         string artista = "GL o EMBAIXADOR vulgo GUSTTAVO LIMA";
+       
+        Console.WriteLine($"Eu sou {nome}, curto o estilo musical {estilo} e meu artista favorito desse estilo é {artista}.");
+        Console.WriteLine();
+
+
+    }
+}
